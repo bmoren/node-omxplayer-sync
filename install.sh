e0807a5b7e945d1e07da8055949f12b74121d67a
Ben Moren <ben@benmoren.com>
2017-12-29 16:57:51 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,21 +16,21 @@ rm ~/install.sh
 source ~/.bashrc 
 nvm install stable 
 
-sudo --yes --force-yes apt-get install git 
+sudo --force-yes apt-get install git 
 git clone https://github.com/bmoren/node-omxplayer-sync.git 
 
 cd ~/node-omxplayer-sync 
 npm install 
 
 #install omxplayer
-sudo --yes --force-yes apt-get install omxplayer 
+sudo --force-yes apt-get install omxplayer 
 
 #install usb-mount so we can easily get files into the system.
-sudo --yes --force-yes apt-get install usbmount 
+sudo --force-yes apt-get install usbmount 
 
 #get your pi up to date!
-sudo --yes --force-yes apt-get update 
-sudo --yes --force-yes apt-get upgrade
+sudo --force-yes apt-get update 
+sudo --force-yes apt-get upgrade
 
 #dont forget to expand your filesystem using:
 #sudo raspi-config
